429b96f0c119de49671bc324c72e42c7beac27cf
hyoni <jih70004@gmail.com>
2024-07-12 22:12:03 +0900

    [React] detail page, github page upload

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,13 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt="coverImage" />
       <h2>
-        <Link to="/movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
-      <p>{summary}</p>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
       <ul>
         {genres.map((g) => (
           <li key={g}>{g}</li>
@@ -18,6 +18,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>
+  const [loading, setLoading] = useState(true);
+  const [detailMovies, setDetailMovies] = useState([]);
+  const { id } = useParams()
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
+
+    setDetailMovies(json.data.movie);
+    setLoading(false);
+  }
+  console.log(detailMovies)
+  useEffect(() => {
+    getMovie();
+  }, [])
+
+  return <div>
+    {loading ? <h1>Loading...</h1> : <div>
+      <img src={detailMovies.medium_cover_image} alt="coverImage" />
+      <h2>{detailMovies.title}</h2>
+      <p>{detailMovies.description_intro}</p>
+      <p>평점: {detailMovies.rating}</p>
+      <p>개봉일: {detailMovies.year}년</p>
+      <p>runtime: {detailMovies.runtime}</p>
+      <p>
+        장르: {detailMovies.genres.map((genres) => (<span> {genres}</span>))}
+      </p>
+    </div>}
+  </div>;
 }
 
 export default Detail;
